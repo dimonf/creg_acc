65bed8962d42b4f0f0aeb634315f7f7b0e1dd348
dimonf <camel109@gmail.com>
2014-11-04 09:39:01 +0200

    simple "direct" insertion: works

--- app/other/insert.test.data.js
+++ app/other/insert.test.data.js
@@ -4,18 +4,15 @@ var	assert = require('assert'),
 	db_collection = 'data_test',
 	async = require('async'),
 	work = async.queue(function(doc, done) {
-		//console.log(doc.doc);
 		doc.db_collection.insert([doc.doc], function(err, result) {
 			if (err) {
 					console.log('error ...' + err);
 			}
-			//assert.equal(err, null);
-			//console.log('error ...' + err);
 		});
 		done();
 	},10),
 	MongoClient = require('mongodb').MongoClient,
-	no_records = 2,
+	no_records = 9999,
 	db_url = 'mongodb://localhost:27017/group_map',
 	get_transaction = require('./transaction.json');
 
@@ -28,15 +25,12 @@ MongoClient.connect(db_url, function(err, db) {
 
 	for (var i=0; i<no_records; i++) {
 		collection.insert(get_transaction(), function(err, result){
-				console.log(err);
-
+				if (err) {
+					console.log(err);
+				} 
 		});
-		//bulk.insert(get_transaction());
 		console.log(['inserted ' , i, ' record'].join(' '));
 
 	}
-	//work.drain = function(err){
-			//db.close();
-	//}
 	console.log('>>inserted ' + no_records + ' records');
 });
